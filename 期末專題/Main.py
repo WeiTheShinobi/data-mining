ab259bb540ed0d4b1192ed9f332b53e629c0f029
WeiTheShinobi <weitheshinobi@gmail.com>
2021-05-30 21:01:48 +0800

    期末專題

---
+++ 期末專題/Main.py
@@ -0,0 +1,5 @@
+import MyTableCreater
+
+if __name__ == '__main__':
+    areas = ['南鯤鯓代天府', '麻豆代天府', '關子嶺溫泉區', '安平小鎮', '奇美博物館']
+    MyTableCreater.create_tourist_table(areas)
---
+++ 期末專題/MyTableCreater.py
@@ -0,0 +1,50 @@
+import os
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.font_manager import FontProperties
+
+
+def create_tourist_table(areas):
+    years = ['2019', '2020']
+    for year in years:
+        file_list = os.listdir('遊客人數/' + year)
+
+        for area in areas:
+            tourist_count_ticket = []
+            tourist_count = []
+
+            for file_name in file_list:
+                file = pd.read_csv('遊客人數/' + year + '/' + file_name)
+                file = file[file['觀光遊憩區別'] == area]
+                try:
+                    tourist_count_ticket.append(int(file['遊客人次有門票_需購票'].values[0].replace(',', '')))
+                except:
+                    tourist_count_ticket.append(0)
+                try:
+                    tourist_count.append(int(file['遊客人次無門票_免費'].values[0].replace(',', '')))
+                except:
+                    tourist_count.append(0)
+
+            __draw_table(tourist_count, tourist_count_ticket, area, year)
+
+
+def __draw_table(tourist_count, tourist_count_ticket, area, year):
+    strChineseFont = 'msj.ttf'
+    myFont = FontProperties(fname=strChineseFont)
+
+    mouth = np.arange(12)
+    y = np.arange(1, len(mouth) + 1)
+    width = 0.3
+    plt.bar(y, tourist_count, label="Free", width=width)
+    plt.bar(y + width, tourist_count_ticket, label="Ticket", width=width)
+
+    ax = plt.subplot(111)
+    box = ax.get_position()
+    ax.set_position([box.x0, box.y0, box.width * 0.8, box.height])
+    ax.legend(loc='center left', bbox_to_anchor=(1, 0.7))
+    plt.xticks(y, mouth)
+    plt.xlabel("月份", fontproperties=myFont)
+    plt.ylabel("遊客數", fontproperties=myFont)
+    plt.title(year + " " + area, fontproperties=myFont)
+    plt.savefig('table/' + area + "_" + year)
